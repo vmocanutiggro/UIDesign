9bb9e34f6176fe15810559e2c0550c938139b86f
Nagalsky Nikita <nagalskyn@gmail.com>
2015-09-30 16:45:04 +0300

    Fix and create 1 more page for sale project

--- sale/src/assets/js/scripts.js
+++ sale/src/assets/js/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+	//Dropdown close button
+	$( ".close-dropdown" ).click(function( event ) {
+		event.preventDefault();
+	});
+
+	//Bootstrap datepicker
+	$('.datepicker-input').datepicker({
+		autoclose: true,
+	    todayHighlight: true,
+	    toggleActive: true
+    });
+
+    $('.input-daterange').datepicker({
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    //Bootstrap timepicker
+   	$('.input-timepicker').timepicker();
+
+
 });
 
 
