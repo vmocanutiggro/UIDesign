ee85690e58b885cc4c7a9be95ae029509fe3b74a
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2016-02-18 20:12:52 +0200

    Metafore project

---
+++ metafore/src/assets/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	//Header sidebar action
+	$('.nav-trigger').click( function (e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$(this).closest('body').toggleClass('aside-open')
+		$(this).closest('body').find('.aside-holder').toggleClass('active');
+		$(this).closest('body').find('.content-holder').toggleClass('active');
+	});
+
+});
+
+
+$(window).load(function(){
+	
+});
+
+
+$(window).resize(function(){
+	
+});
