597fd616f5dbc4ed66e0d476235bc3cffc5e1bbe
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2016-03-21 15:43:04 +0200

    Fix images and logos for Metafore project

--- metafore/src/assets/js/scripts.js
+++ metafore/src/assets/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	//Header sidebar action
+	/* ======== SCROLL TO ELEMENT ======== */
 	$('.nav-trigger').click( function (e) {
 		e.preventDefault();
 		$(this).toggleClass('active');
@@ -8,6 +8,17 @@ $(document).ready(function(){
 		$(this).closest('body').find('.aside-holder').toggleClass('active');
 		//$(this).closest('body').find('.content-holder').toggleClass('active');
 	});
+
+	/* ======== SCROLL TO ELEMENT ======== */
+	$('[data-click=scroll-to-target]').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var target = $(this).attr('href');
+        var headerHeight = 0;
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - headerHeight
+        }, 500);
+    });
 
 });
 
