c1446f839af2509003b84f706d12b9e9ce828cd1
Nagalsky Nikita <nagalsky.nikita@gmail.com>
2016-03-30 17:52:31 +0300

    YugglrFB project

---
+++ yugglr-fb/src/assets/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	/* ======== SCROLL TO ELEMENT ======== */
+	// $('[data-click=scroll-to-target]').on('click', function(e) {
+ //        e.preventDefault();
+ //        e.stopPropagation();
+ //        var target = $(this).attr('href');
+ //        var headerHeight = 0;
+ //        $('html, body').animate({
+ //            scrollTop: $(target).offset().top - headerHeight
+ //        }, 500);
+ //    });
+
+});
+
+
+$(window).load(function(){
+	
+});
+
+
+$(window).resize(function(){
+	
+});
